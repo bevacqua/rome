969b0ed101a669c4e1f32c17b3192eef2fc5d6be
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-07-20 14:37:16 -0300

    Release v0.3.6

--- dist/rome.js
+++ dist/rome.js
@@ -1,6 +1,6 @@
 /**
  * rome - Dependency free, opt-in UI, customizable date _(and time)_ picker!
- * @version v0.3.5
+ * @version v0.3.6
  * @link https://github.com/bevacqua/rome
  * @license MIT
  */
