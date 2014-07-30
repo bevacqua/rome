55413b6cba55eb07dfc97a8199892ef8db1825be
Jolie O'Dell <odell.jolie@gmail.com>
2014-07-29 18:39:01 -0700

    added disallow to example html page

--- example/example.js
+++ example/example.js
@@ -13,5 +13,5 @@ toggle.addEventListener('click', function () {
   toggle.innerHTML = picker.restore ? 'Restore <code>rome</code> instance!' : 'Destroy <code>rome</code> instance!';
 });
 
-rome(mm, { min: '2013-12-30', max: '2014-10-01' });
-rome(mmt, { min: '2014-04-30 19:45', max: '2014-09-01 08:30' });
+rome(mmd, { min: '2013-12-30', max: '2014-10-01', time: false, disallow: ['2014-01-15']});
+rome(mmt, { min: '2014-04-30 19:45', max: '2014-09-01 08:30', disallow: ['2014-05-15 09:00'] });
